4d19f892c138693d63b99f7a977ebe7ddf538b5f
Marco <87058218+MaPaSoTo@users.noreply.github.com>
2023-02-01 17:23:58 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -5,5 +5,5 @@ chmod 777 /home/nodered
 mkdir /home/nodered/.node-red
 chmod 777 /home/nodered/.node-red
 cd /home/nodered/.node-red
-npm install node-red-dashboard node-red-contrib-azure-iot-central node-red-contrib-azure-iot-device node-red-node-random node-red-node-rbe ttb-node-red-counter
-cp flow.json /home/nodered/.node-red/flow.json
+npm install node-red-dashboard node-red-contrib-azure-iot-central node-red-contrib-azure-iot-device node-red-node-random ttb-node-red-counter
+cp flow.json /home/nodered/.node-red/flows.json
