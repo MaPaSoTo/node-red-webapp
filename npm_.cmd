e4b986c97a1d99509ab34c6f79c253b1b461ba15
Marco <87058218+MaPaSoTo@users.noreply.github.com>
2024-06-20 15:00:58 +0200

    Update npm_.cmd

--- npm_.cmd
+++ npm_.cmd
@@ -1,5 +1,5 @@
 :: Created by npm, please don't edit manually.
-::@ECHO OFF
+@ECHO OFF
 
 SETLOCAL
 
@@ -7,8 +7,8 @@ SET "NODE_EXE=%~dp0\node.exe"
 IF NOT EXIST "%NODE_EXE%" (
   SET "NODE_EXE=node"
 )
-:: Orig npm was 5.6.0 -> 8.19.2
-SET "NPM_CLI_JS=d:\program files (x86)\npm\8.19.2\node_modules\npm\bin\npm-cli.js"
+
+SET "NPM_CLI_JS=d:\program files (x86)\npm\9.7.2\node_modules\npm\bin\npm-cli.js"
 FOR /F "delims=" %%F IN ('CALL "%NODE_EXE%" "%NPM_CLI_JS%" prefix -g') DO (
   SET "NPM_PREFIX_NPM_CLI_JS=%%F\node_modules\npm\bin\npm-cli.js"
 )
