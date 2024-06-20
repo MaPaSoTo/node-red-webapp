b8a1cf47c9327ff56fc4a68e737d9719eb9f7008
Marco <87058218+MaPaSoTo@users.noreply.github.com>
2024-06-20 10:00:49 +0200

    Update postinstall.cmd
    update packages

--- postinstall.cmd
+++ postinstall.cmd
@@ -4,9 +4,11 @@
 copy npm_.cmd \home\nodered\.node-red\npm_.cmd
 copy flow.json \home\nodered\.node-red\flow.json
 pushd \home\nodered\.node-red
-npm install node-red-dashboard node-red-contrib-azure-iot-central node-red-contrib-azure-iot-device node-red-node-random ttb-node-red-counter & (
+npm install node-red-dashboard node-red-contrib-azure-iot-central node-red-contrib-azure-iot-device node-red-node-random ttb-node-red-counter node-red-contrib-aedes node-red-contrib-loop node-red-contrib-opcua node-red-contrib-opcua-server & (
      copy npm_.cmd npm.cmd /y
      copy flow.json flows.json /y
      popd 
      copy npm_.cmd npm.cmd /y
 )
+
+
