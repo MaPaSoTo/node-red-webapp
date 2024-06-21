4e290b21ac3f97f98c2fdc4696cc60e742dba5af
Marco <87058218+MaPaSoTo@users.noreply.github.com>
2024-06-21 13:22:56 +0200

    Update postinstall.cmd

--- postinstall.cmd
+++ postinstall.cmd
@@ -2,11 +2,9 @@
 (if not exist \home\nodered mkdir \home\nodered)
 (if not exist \home\nodered\.node-red mkdir \home\nodered\.node-red)
 copy npm_.cmd \home\nodered\.node-red\npm_.cmd
-copy flow.json \home\nodered\.node-red\flow.json
 pushd \home\nodered\.node-red
-npm install node-red-dashboard node-red-contrib-azure-iot-central node-red-contrib-azure-iot-device node-red-node-random ttb-node-red-counter node-red-contrib-aedes node-red-contrib-loop node-red-contrib-opcua & (
+npm install node-red-dashboard & (
      copy npm_.cmd npm.cmd /y
-     copy flow.json flows.json /y
      popd 
      copy npm_.cmd npm.cmd /y
 )
