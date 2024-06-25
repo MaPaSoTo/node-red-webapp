35caf36a66567890bdf76f606848874378e97205
Marco <87058218+MaPaSoTo@users.noreply.github.com>
2024-06-25 17:00:46 +0200

    Create example_postinstall.sh with some nodes and a sample flow

---
+++ example_postinstall.sh
@@ -0,0 +1,8 @@
+mkdir /home/nodered
+chmod 777 /home/nodered
+mkdir /home/nodered/.node-red
+chmod 777 /home/nodered/.node-red
+cd /home/nodered/.node-red
+cp flow.json /home/nodered/.node-red/flows.json
+npm install node-red-dashboard node-red-contrib-azure-iot-central node-red-contrib-azure-iot-device node-red-node-random ttb-node-red-counter node-red-contrib-aedes node-red-contrib-loop node-red-contrib-opcua
+cp flow.json /home/nodered/.node-red/flows.json
